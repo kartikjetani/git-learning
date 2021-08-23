7c378cf6d612e6821548ac33d02a92717b04b9da
Brijesh Patadiya <brijeshsoni1300@gmail.com>
2021-08-23 19:53:26 +0530

    Update test.py

--- test.py
+++ test.py
@@ -6,6 +6,14 @@ def sum(a,b):
 def divide(x,y):
     return x//y
 
+
+def test(a):
+    print(a)
+
+
+test(5)
+
+
 print(divide(8,3))
 
 print(sum(5,6))
@@ -14,3 +22,5 @@ def mul(a,b):
     return a*b
 
 print(mul(5,6))
+
+
